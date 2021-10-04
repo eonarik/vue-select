b1d2990400630cca92355647ce89ae507df47e7e
oleg.kosnyrev <eonarik@mail.ru>
2021-10-04 16:21:19 +0300

    initial commit :)

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.layout
+  Select
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+import { Select } from '@/components';
+
+@Options({
+  components: {
+    Select,
+  },
+})
+export default class Home extends Vue {}
+</script>
